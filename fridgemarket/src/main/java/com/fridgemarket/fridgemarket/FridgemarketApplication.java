0236adf599ce77906fa8c3e1680f69c1a833aec6
이수종 <leesj9627@naver.com>
2025-06-29 21:37:52 +0900

    Initial commit

---
+++ fridgemarket/src/main/java/com/fridgemarket/fridgemarket/FridgemarketApplication.java
@@ -0,0 +1,13 @@
+package com.fridgemarket.fridgemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgemarketApplication.class, args);
+	}
+
+}
---
+++ fridgemarket/src/test/java/com/fridgemarket/fridgemarket/FridgemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fridgemarket.fridgemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
