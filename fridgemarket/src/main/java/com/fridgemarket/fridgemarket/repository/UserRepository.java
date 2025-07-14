122585da5a22902d5b95bc325034a90f67480b5f
이수종 <leesj9627@naver.com>
2025-07-14 19:00:53 +0900

    Initial commit

--- fridgemarket/src/main/java/com/fridgemarket/fridgemarket/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fridgemarket.fridgemarket.repository;
-import com.fridgemarket.fridgemarket.DAO.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
